9ab01a2265c0f14222db79a086744d94c2079476
tallivm <animbuss@gmail.com>
2022-12-13 19:02:06 +0200

    added dateparser and logprobs parameter

--- rest-api/src/gpt3.py
+++ rest-api/src/gpt3.py
@@ -1,21 +1,15 @@
 from datetime import datetime
 import openai
-
+from dateparser.search import search_dates
 from typing import List, Tuple, TypedDict, Optional
 
 
-INITIAL_PROMPT_PLACEHOLDER = ("I will give you a sentence using which you need to generate 1-3 tags useful for "
-                              "categorizing it in a calendar or a to-do list. Tags should be comma-separated. If you "
-                              "found a date, output the date in 'year-month-day hour:minutes' format after a newline. "
-                              "The sentence: {sentence}")
-PROCESSING_TAGS_PLACEHOLDERS = [
-    "I will give you comma-separated tags, check if some of them are synonymous and return new comma-separated tags "
-    "which are more standard and useful for creating calendar events of to-do lists. Tags: {tags}",
-]
+INITIAL_PROMPT_PLACEHOLDER = ("Generate up to 3 categories from a given sentence. Categories should be comma-separated."
+                              "A sentence that reminds of any event should have 'reminder' category."
+                              "Sentence: {sentence}")
+PROCESSING_TAGS_PLACEHOLDERS = []
 
-PROCESSING_MODELS = [
-    'text-davinci-003',
-]
+PROCESSING_MODELS = []
 
 Response = str
 
@@ -25,28 +19,31 @@ class Record(TypedDict):
     tags: List[str]
 
 
-def get_response(prompt: str, model: str, temperature: float = 0.0,
-                 frequency_penalty: float = 0.0, presence_penalty: float = 0.0,
-                 top_p: float = 1.0, max_tokens: int = 64) -> Response:
-    response = openai.Completion.create(
+def get_response(prompt: str, model: str, temperature: float = 0.0, frequency_penalty: float = 0.0,
+                 presence_penalty: float = 0.0, top_p: float = 0.0, logprobs: int = 1,
+                 max_tokens: int = 16) -> Response:
+    answer = openai.Completion.create(
         model=model,
         prompt=prompt,
         temperature=temperature,
         max_tokens=max_tokens,
         top_p=top_p,
         frequency_penalty=frequency_penalty,
-        presence_penalty=presence_penalty
+        presence_penalty=presence_penalty,
+        logprobs=logprobs
     )
-    return response['choices'][0]['text'].split('\n\n')[1].strip()
+    responses = [answer['choices'][i]['text'].split('\n\n')[1].strip() for i in range(logprobs)]
+    return responses[0]  # TODO: filter responses instead of choosing the with one
 
 
 def get_initial_response(sentence: str, prompt_placeholder: str, model: str) -> Response:
     prompt = prompt_placeholder.format(sentence=sentence)
-    return get_response(prompt, model=model)
+    response = get_response(prompt, model=model)
+    return response
 
 
 def response2tags(response: Response) -> List[str]:
-    tags = [x.strip() for x in response.split(',')]  # TODO: rewrite to work with actual responses
+    tags = [x.strip() for x in response.split(',')]
     return tags
 
 
@@ -54,18 +51,16 @@ def tags2input(tags: List[str]) -> str:
     return ', '.join(tags)
 
 
-def split_tags_from_date(raw_tags: List[str], date_format: str = '%Y-%m-%d %H:%M') -> Tuple[List[str], Optional[datetime]]:
-    last_tag = raw_tags[-1]
-    try:
-        date = datetime.strptime(last_tag, date_format)
-        tags = raw_tags[:-1]
-    except ValueError:
-        tags = raw_tags
-        date = None
-    return tags, date
+def split_tags_from_date(response: Response) -> Tuple[str, Optional[datetime]]:
+    date = search_dates(response, languages=['en'])[0]  # TODO: change language by user's preference, or auto-detect
+    if date is None:
+        return response, None
+    else:
+        response_wo_date = response.replace(date[0], '')
+        return response_wo_date, date[1]
 
 
-def finalize_tags(raw_tags: List[str], prompt_placeholders: List[str], models: List[str]) -> List[str]:
+def finalize_tags_with_gpt3(raw_tags: List[str], prompt_placeholders: List[str], models: List[str]) -> List[str]:
     tags = raw_tags
     for placeholder, model in zip(prompt_placeholders, models):
         response = get_response(placeholder.format(tags=tags2input(tags)), model=model)
@@ -77,8 +72,8 @@ def get_tags_and_date(sentence: str, model: str = 'text-davinci-003') -> Record:
     with open('key.key', 'r') as f:
         openai.api_key = f.read()
     response = get_initial_response(sentence, INITIAL_PROMPT_PLACEHOLDER, model=model)
-    raw_tags = response2tags(response)
-    raw_tags, date = split_tags_from_date(raw_tags, '%Y-%m-%d %H:%M')
-    tags = finalize_tags(raw_tags, PROCESSING_TAGS_PLACEHOLDERS, PROCESSING_MODELS)
+    response_wo_date, date = split_tags_from_date(response)
+    raw_tags = response2tags(response_wo_date)
+    tags = finalize_tags_with_gpt3(raw_tags, PROCESSING_TAGS_PLACEHOLDERS, PROCESSING_MODELS)
     record: Record = {'date': date, 'tags': tags}
     return record
