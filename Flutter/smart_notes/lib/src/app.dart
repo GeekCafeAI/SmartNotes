e241cafe6dbece55abb7a1d567bf22bc489ed043
maksiito <maksim.fisenko@gmail.com>
2022-12-16 20:19:21 +0200

    First mvp commit

--- Flutter/smart_notes/lib/src/app.dart
+++ Flutter/smart_notes/lib/src/app.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
+import 'screens/main_menu.dart';
+import 'screens/todos_page.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
 
@@ -69,11 +69,13 @@ class MyApp extends StatelessWidget {
                 switch (routeSettings.name) {
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
+                  case TodoPage.routeName:
+                    return TodoPage(
+                      settingsController: settingsController,
+                    );
+                  case MainMenu.routeName:
                   default:
-                    return const SampleItemListView();
+                    return const MainMenu();
                 }
               },
             );
--- rest-api/src/gpt3.py
+++ rest-api/src/gpt3.py
@@ -1,23 +1,57 @@
 from datetime import datetime
 import openai
+from dateparser.search import search_dates
+from typing import List, TypedDict, Optional, Tuple
+from pathlib import Path
 
-from typing import List, Tuple, TypedDict, Optional
+
+PATH_TO_KEY_FILE = Path().resolve() / Path("rest-api", "src", "key.key")
+
+INITIAL_TAG_PROMPT_PLACEHOLDER = (
+    "Generate up to 4 categories from the given text. The categories should be comma-separated and end with '###'. "
+    "Add the 'reminder' category for the text that mentions any event in the future. Add the 'idea' category for the "
+    "text that describes an idea. Add the 'goal' category for the text that describes a wish or personal goal. For the "
+    "text that includes proper nouns, add them as categories, but do not add human names. Do not continue the text or "
+    "write duplicated categories, and ignore dates."
+    "\nText: I have a meeting with John on January 2nd, 2022.###"
+    "\nCategories: reminder, meeting###"
+    "\nText: I need to buy tomatoes until Monday.###"
+    "\nCategories: groceries, shopping, reminder###"
+    "\nText: Learn how to write mobile apps with Flutter and Python###"
+    "\nCategories: goal, learning, programming###"
+    "\nText: Lila's birthday: December 20.###"
+    "\nCategories: birthday###"
+    "\nText: {sentence}###\nCategories:)"
+)
+
+INITIAL_DATE_PROMPT_PLACEHOLDER = (
+    "Find a date in the provided text and output it in year-month-day format. Add the 'recurrent' word after a comma "
+    "for the text that includes a recurrent event."
+    "\nText: I have a meeting with John on January 2nd, 2022.###"
+    "\nCategories: 2022-01-02###"
+    "\nText: I need to buy tomatoes until Monday.###"
+    "\nCategories: Monday###"
+    "\nText: Learn how to write mobile apps with Flutter and Python###"
+    "\nCategories:###"
+    "\nText: Lila's birthday: December 20.###"
+    "\nCategories: recurrent, december 20###"
+    "\nText: {sentence}###\nCategories:"
+)
 
 
-INITIAL_PROMPT_PLACEHOLDER = ("I will give you a sentence using which you need to generate 1-3 tags useful for "
-                              "categorizing it in a calendar or a to-do list. Tags should be comma-separated. If you "
-                              "found a date, output the date in 'year-month-day hour:minutes' format after a newline. "
-                              "The sentence: {sentence}")
-PROCESSING_TAGS_PLACEHOLDERS = [
-    "I will give you comma-separated tags, check if some of them are synonymous and return new comma-separated tags "
-    "which are more standard and useful for creating calendar events of to-do lists. Tags: {tags}",
-]
+PROCESSING_TAGS_PLACEHOLDERS = []
+PROCESSING_MODELS = []
+DATE_TAGS = ('recurrent',)
 
-PROCESSING_MODELS = [
-    'text-davinci-003',
-]
+
+DATEPARSER_SETTINGS = {
+    'PREFER_DATES_FROM': 'future',
+    'PARSERS': ['relative-time', 'absolute-time'],
+    'RETURN_TIME_AS_PERIOD': True
+}
 
 Response = str
+Tags = list[str]
 
 
 class Record(TypedDict):
@@ -25,47 +59,60 @@ class Record(TypedDict):
     tags: List[str]
 
 
-def get_response(prompt: str, model: str, temperature: float = 0.0,
-                 frequency_penalty: float = 0.0, presence_penalty: float = 0.0,
-                 top_p: float = 1.0, max_tokens: int = 64) -> Response:
-    response = openai.Completion.create(
+def get_response(prompt: str, model: str, temperature: float = 0.0, frequency_penalty: float = 0.0,
+                 presence_penalty: float = 0.0, top_p: float = 0.0, logprobs: int = 1,
+                 max_tokens: int = 64) -> Response:
+    request = openai.Completion.create(
         model=model,
         prompt=prompt,
         temperature=temperature,
         max_tokens=max_tokens,
         top_p=top_p,
         frequency_penalty=frequency_penalty,
-        presence_penalty=presence_penalty
+        presence_penalty=presence_penalty,
+        logprobs=logprobs,
+        stop='###'
     )
-    return response['choices'][0]['text'].split('\n\n')[1].strip()
+    responses = [request['choices'][i]['text'].strip() for i in range(logprobs)]
+    best_response = filter_responses(responses)
+    return best_response
+
+
+def filter_responses(responses: List[Response]) -> Response:
+    # TODO: filter responses instead of choosing the first one
+    return responses[0]
 
 
 def get_initial_response(sentence: str, prompt_placeholder: str, model: str) -> Response:
     prompt = prompt_placeholder.format(sentence=sentence)
-    return get_response(prompt, model=model)
+    response = get_response(prompt, model=model)
+    return response
 
 
-def response2tags(response: Response) -> List[str]:
-    tags = [x.strip() for x in response.split(',')]  # TODO: rewrite to work with actual responses
-    return tags
+def response2tags(response: Response) -> Tags:
+    tags = [x.strip() for x in response.split(',')]
+    return [x for x in tags if x]
 
 
-def tags2input(tags: List[str]) -> str:
+def response2date(response: Response, language: str) -> Optional[datetime]:
+    parsed_date = search_dates(response, languages=[language])
+    if parsed_date is not None:
+        return parsed_date[0][1]
+
+
+def response2date_tags(response: Response, date_tags: Tuple[str] = DATE_TAGS) -> Tags:
+    found_date_tags = []
+    for tag in date_tags:
+        if tag in response:
+            found_date_tags.append(tag)
+    return found_date_tags
+
+
+def tags2input(tags: Tags) -> str:
     return ', '.join(tags)
 
 
-def split_tags_from_date(raw_tags: List[str], date_format: str = '%Y-%m-%d %H:%M') -> Tuple[List[str], Optional[datetime]]:
-    last_tag = raw_tags[-1]
-    try:
-        date = datetime.strptime(last_tag, date_format)
-        tags = raw_tags[:-1]
-    except ValueError:
-        tags = raw_tags
-        date = None
-    return tags, date
-
-
-def finalize_tags(raw_tags: List[str], prompt_placeholders: List[str], models: List[str]) -> List[str]:
+def finalize_tags_with_gpt3(raw_tags: Tags, prompt_placeholders: List[str], models: List[str]) -> Tags:
     tags = raw_tags
     for placeholder, model in zip(prompt_placeholders, models):
         response = get_response(placeholder.format(tags=tags2input(tags)), model=model)
@@ -73,12 +120,47 @@ def finalize_tags(raw_tags: List[str], prompt_placeholders: List[str], models: L
     return [x.lower() for x in tags]
 
 
-def get_tags_and_date(sentence: str, model: str = 'text-davinci-003') -> Record:
-    with open('key.key', 'r') as f:
+def get_tags_and_date(sentence: str, language: str = 'en', model: str = 'text-davinci-003',
+                      tag_prompt_placeholder: str = INITIAL_TAG_PROMPT_PLACEHOLDER,
+                      date_prompt_placeholder: str = INITIAL_DATE_PROMPT_PLACEHOLDER, verbose: bool = False) -> Record:
+    if not sentence:
+        return {'date': None, 'tags': []}
+    with open(PATH_TO_KEY_FILE, 'r') as f:
         openai.api_key = f.read()
-    response = get_initial_response(sentence, INITIAL_PROMPT_PLACEHOLDER, model=model)
-    raw_tags = response2tags(response)
-    raw_tags, date = split_tags_from_date(raw_tags, '%Y-%m-%d %H:%M')
-    tags = finalize_tags(raw_tags, PROCESSING_TAGS_PLACEHOLDERS, PROCESSING_MODELS)
+    tag_response = get_initial_response(sentence, prompt_placeholder=tag_prompt_placeholder, model=model)
+    date_response = get_initial_response(sentence, prompt_placeholder=date_prompt_placeholder, model=model)
+    if verbose:
+        print('Tags:', tag_response)
+        print('Date:', date_response)
+    raw_tags = response2tags(tag_response)
+    date = response2date(date_response, language=language)
+    date_tags = response2date_tags(date_response)
+    tags = finalize_tags_with_gpt3(raw_tags + date_tags, PROCESSING_TAGS_PLACEHOLDERS, PROCESSING_MODELS)
     record: Record = {'date': date, 'tags': tags}
     return record
+
+
+if __name__ == "__main__":
+
+    test_questions = [
+        'buy sour cream until monday',
+        '1on1 meeting with the boss friday next week',
+        'I want to get fit by the end of year',
+        'BananaShop project deadline is 12th October',
+        'Sasha birthday Nov 20',
+        'idea for a new character for my book: guy who never sleeps',
+        'Take pills 3 times a day',
+        'I felt derealisation just now'
+    ]
+    for question in test_questions:
+        print(question)
+        print(get_tags_and_date(question, verbose=True), '\n')
+
+    while True:
+        question = input('Enter your sentence: ')
+        if question == 'stop':
+            print('Stopped testing.')
+            break
+        if question:
+            answer = get_tags_and_date(question, verbose=True)
+            print(answer)
