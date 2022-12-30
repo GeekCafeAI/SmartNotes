6c9116e1e9913eb206c2783000220e7f5d16e634
maksiito <maksim.fisenko@gmail.com>
2022-12-30 14:44:11 +0200

    Improved state management and added ways to access tags/filter everywhere in the app

--- Flutter/smart_notes/lib/src/providers/tasks.dart
+++ Flutter/smart_notes/lib/src/providers/tasks.dart
@@ -4,13 +4,39 @@ import '../models/task_model.dart';
 
 class Tasks with ChangeNotifier {
   final List<Task> _items = [];
+  final List<String> _tags = [];
 
   List<Task> get items {
     return [..._items];
   }
 
+  List<String> get tags {
+    return [..._tags.toSet().toList()];
+  }
+
   void addTask(Task value) {
     _items.add(value);
     notifyListeners();
+  }
+
+  void assignTags() {
+    List<String> tagsList = [];
+    for (var element in _items) {
+      var tagsString = element.tags;
+      tagsList.addAll(splitTags(tagsString));
+    }
+
+    _tags.addAll(tagsList);
+  }
+
+  // Converts comma separated string into a list
+  List<String> splitTags(String tags) {
+    final splitTags = tags.split(",");
+    // final List<String> myTags = [];
+    // for (int i = 0; i < splitTags.length; i++) {
+    //   myTags.add(splitTags[i]);
+    // }
+
+    return splitTags;
   }
 }
